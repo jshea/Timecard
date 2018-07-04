1d95a7253ba601ce70cc8fe396357407e38ba803
Jim Shea <jshea@jpl.nasa.gov>
2018-07-04 08:14:56 -0700

    Convert cli main component to a material nav

--- src/app/timecard.component.ts
+++ src/app/timecard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'tc-timecard',
@@ -6,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./timecard.component.css']
 })
 export class TimecardComponent {
-  title = 'Timecard';
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
