191aea5be9e4217fd1581946d65fb14f00e594e0
Jim Shea <jshea@jpl.nasa.gov>
2018-07-04 08:12:44 -0700

    Add hammer & moment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+// https://material.angular.io/guide/getting-started#step-5-gesture-support
+import 'hammerjs';
+
 import { TimecardModule } from './app/timecard.module';
 import { environment } from './environments/environment';
 
