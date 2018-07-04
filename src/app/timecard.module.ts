8f63750894efc819db927f500d9b854b64dcb612
Jim Shea <jshea@jpl.nasa.gov>
2018-07-04 08:13:19 -0700

    Move material imports to it's own module

--- src/app/timecard.module.ts
+++ src/app/timecard.module.ts
@@ -1,37 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MaterialModule } from './shared/material.module';
+
 import { TimecardRoutingModule } from './timecard-routing.module';
 import { TimecardComponent } from './timecard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule,
-         MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 import { TableComponentComponent } from './table-component/table-component.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MaterialModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    TimecardRoutingModule
+  ],
   declarations: [
     TimecardComponent,
     MainNavComponent,
     TableComponentComponent
-  ],
-  imports: [
-    BrowserModule,
-    TimecardRoutingModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [
