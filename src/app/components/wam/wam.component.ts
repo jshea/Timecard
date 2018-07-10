ed363712973bb852339ff603dd2aaa2212dd6523
Jim Shea <jshea@jpl.nasa.gov>
2018-07-10 14:49:36 -0700

    Updates to WAM table. Simplify it.

--- src/app/components/wam/wam.component.ts
+++ src/app/components/wam/wam.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, OnInit, Input, ViewChild, AfterViewInit, SimpleChanges } from '@angular/core';
+import { Component, OnChanges, OnInit, Input, ViewChild, SimpleChanges } from '@angular/core';
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 
 import * as moment from 'moment';
@@ -10,7 +10,7 @@ import { WAM } from '../../shared/classes/wam';
   templateUrl: './wam.component.html',
   styleUrls:  ['./wam.component.css']
 })
-export class WamComponent implements OnChanges, OnInit, AfterViewInit {
+export class WamComponent implements OnChanges, OnInit {
   @Input() wams: WAM[] = [];
 
   public displayedColumns = ['project', 'projectName', 'task', 'taskName', 'from', 'to'];
@@ -26,22 +26,16 @@ export class WamComponent implements OnChanges, OnInit, AfterViewInit {
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['wams'] && changes['wams'].currentValue) {
       this.dataSource = new MatTableDataSource<WAM>(this.wams);
-    }
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      }
   }
 
   ngOnInit() { }
 
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-
-
   applyFilter(filterValue: string) {
-    filterValue = filterValue.trim();         // Remove whitespace
-    filterValue = filterValue.toLowerCase();  // MatTableDataSource defaults to lowercase matches
-    this.dataSource.filter = filterValue;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
